d390f6d363aad40de000b3f2ce4c3d552c69192d
Claudia Romeo <claudiaromeo@MacBook-Pro-di-Claudia.local>
2017-07-18 09:24:42 +0200

    draw nodes

--- l_draw.js
+++ l_draw.js
@@ -1,4 +1,3 @@
-
 var svg = d3.select("body")
 			.append("svg")
 			.attr("width",960)
@@ -24,21 +23,15 @@ function draw_planar_graph(nodes, edges){
 	var dy = 60
 
 	var x_coordinates = []
-	for(var i=dx; i<width; i+=dx){
+	for(var i=dx; i<430; i+=dx){
 		x_coordinates.push(i)
 	}
 	var y_coordinates = []
-	for (var i = dy; i <height; i+=dy) {
+	for (var i = dy; i<600; i+=dy) {
 		y_coordinates.push(i)
 	}
 
-	var points = create_points_list(x_coordinates, y_coordinates);
-	console.log(points.length)
-	
-
-	//console.log(x_coordinates)
-	//console.log(y_coordinates)
-
+	var lineGenerator = d3.line().curve(d3.curveCatmullRomOpen);
 
 	var numNodi = nodes.length;
 	
@@ -54,31 +47,36 @@ function draw_planar_graph(nodes, edges){
 				.attr("id", function(d){return d.source+d.target})
 				.attr("source", function(d){return d.source})
 				.attr("target", function(d){return d.target})
-				.attr("stroke", "grey")
+				.attr("stroke", "#dbdde6")
 				.attr("stroke-width", 5);
 
 
 	var node = nodeGroup.selectAll("circle")
 				.data(nodes)
 				.enter()
-				.append("circle")
-				.attr("id", function(d){return d.id})
-				.attr("cx", function(d){
-					var x = points[Math.floor(Math.random()*points.length)].x
-					return x;
-				})
-				.attr("cy", function(d){
-					var x = d3.select(this).attr("cx");
-					var y = points[Math.floor(Math.random()*points.length)].y
-					removeElem(points, x,y);
-					return y
-				})
-				.attr("r", 15)
-				.attr("fill", "black")
-				.attr("stroke", "white")
-				.attr("stroke-width", 5);
-	console.log(points.length);
-	console.log(points);
+				.each(function(d) {
+					var header = d3.select(this);
+		            var x_index = Math.floor(Math.random()*x_coordinates.length);
+		            var y_index = Math.floor(Math.random()*y_coordinates.length);
+		            
+		            if((x_coordinates[x_index] != null) && (y_coordinates[y_index] != null)){
+		            	var circle = header.append("circle").attr("id",function(d){return d.id})
+		            	
+		            	circle.attr("cx", x_coordinates[x_index]);
+		            	x_coordinates.splice(x_index, 1);
+		            	
+		            	circle.attr("cy", y_coordinates[y_index]);
+		            	y_coordinates.splice(y_index, 1);
+		            	
+		            	circle.attr("r", 15)
+						circle.attr("fill", "#095ae0")
+						circle.attr("stroke", "white")
+						circle.attr("stroke-width", 5)
+		            }
+		        	});
+
+
+
 
 	edge.attr("x1", function(d){
 			return d3.select("circle[id='"+d.source+"']").attr("cx")
@@ -93,8 +91,6 @@ function draw_planar_graph(nodes, edges){
 			return d3.select("circle[id='"+d.target+"']").attr("cy")
 		});
 	
-	//edge.curve(d3.curveStepBefore);
-
 }
 
 function create_points_list(x_coordinates, y_coordinates){
@@ -102,21 +98,11 @@ function create_points_list(x_coordinates, y_coordinates){
 
 	for (var i = 0; i < x_coordinates.length; i++) {
 		for (var j = 0; j < y_coordinates.length; j++) {
-			var point = new Point(x_coordinates[i], y_coordinates[j]);
-			result.push(point);
+
+			result.push([x_coordinates[i],y_coordinates[j]]);
 
 		}
 	}
 	return result;
-}
-
-function removeElem(points, x, y){
-	for (var i = 0; i < points.length; i++) {
-		if(points[i].x == x | points[i].y == y){
-			console.log("Rimosso: ("+ points[i].x +","+points[i].y+")");
-			points.splice(i, 1);
-
-		}
-	}
 }
 
