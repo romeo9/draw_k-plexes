4cecaac2e085cb395ce6a1b491e808dc7023fc49
Antonio Martinelli <antonio.martinelli93@gmail.com>
2017-08-26 18:32:35 +0200

    code to make Claudia happy

---
+++ new_drawing.js
@@ -0,0 +1,157 @@
+canvasWidth = (screen.width)/100*98
+canvasHeight = (screen.height)/100*65
+
+var baseCanvas = d3.select("body")
+					.append("svg")
+					.attr("id","canvas")
+					.attr("width", canvasWidth)
+					.attr("height", canvasHeight)
+					.attr("border",1)
+
+var canvasBorder = baseCanvas.append("rect")
+       			.attr("x", 0)
+       			.attr("y", 0)
+       			.attr("height", canvasHeight)
+       			.attr("width", canvasWidth)
+       			.style("stroke", 'black')
+       			.style("fill", "none")
+       			.style("stroke-width", 1);
+
+var width = baseCanvas.attr("width");
+var height = baseCanvas.attr("height");
+
+d3.json("ndeConverter/ca-GrQc.json", function(error, data){
+	if(error) throw error;
+	nodes = data.nodes;
+	edges = data.links;
+
+	var navbar = d3.select("header").select(".topnav");
+    navbar.append("div").text("Numero nodi: "+nodes.length)
+    navbar.append("div").text("Numero archi: "+edges.length)
+	
+	draw_plexes(nodes, edges);
+});
+
+function draw_plexes(nodes, edges){
+
+	d3.text("2-plexes/cluster_output_ca-GrQc_2_35.csv", function(error, data) {
+       	if(error) throw error;
+       	plexes = d3.csvParseRows(data);
+       	plexes.splice(-1,1)
+		
+		plexes.sort(function(a, b){
+			return b.length - a.length;
+    	});
+
+    	biggestPlexLength = plexes[0].length;
+
+		var xRangeMin = width/4;
+		var xRangeMax = (width/4)*3;
+
+		var yRangeMin = (height/4);
+		var yRangeMax = (height/4)*3;
+
+		var dx = (width/2)/biggestPlexLength;
+		var dy = (height/2)/biggestPlexLength;
+
+		var x_coordinates = []
+		for(var i = xRangeMin; i < xRangeMax; i += dx){
+			x_coordinates.push(i)
+		}
+		var y_coordinates = []
+		for (var i = yRangeMin; i < yRangeMax; i += dy) {
+			y_coordinates.push(i)
+		}
+
+		var edgeGroup = baseCanvas.append("g")
+									.attr("id", "edges");
+		var nodeGroup = baseCanvas.append("g")
+									.attr("id", "nodes");
+
+		var edge = edgeGroup.selectAll("path")
+				.data(edges)
+				.enter()
+				.append("path")
+				.attr("id", function(d){return d.source+"-"+d.target})
+				.attr("source", function(d){return d.source})
+				.attr("target", function(d){return d.target})
+				.attr("stroke", "#dbdde6")
+				.attr("stroke-width", 1)
+				.attr("fill", "transparent");
+
+		var node = nodeGroup.selectAll("circle")
+		.data(plexes[0])
+		.enter()
+		.each(function(d) {
+			var header = d3.select(this);
+            var x_index = Math.floor(Math.random()*x_coordinates.length);
+            var y_index = Math.floor(Math.random()*y_coordinates.length);
+            
+            if((x_coordinates[x_index] != null) && (y_coordinates[y_index] != null)) {
+
+            	var cx = x_coordinates[x_index];
+            	var cy = y_coordinates[y_index];
+
+            	var circleNode = header.append("g")
+            							.attr("id",function(d){return d})
+            							.on("mouseover", handleMouseOver)
+										.on("mouseout", handleMouseOut);
+
+            	circleNode.append("circle")
+            				.attr("id", "circle" + d)
+            				.attr("cx", cx)
+							.attr("cy", cy)
+							.attr("r", dy/2)
+							.attr("fill", "red")
+							.attr("stroke", "white")
+							.attr("stroke-width", dy/4)
+							
+            	
+            	x_coordinates.splice(x_index, 1);
+            	y_coordinates.splice(y_index, 1);
+            	
+            	circleNode.append("text")
+					.text(function(d) {return d;})
+					.attr("id", "node_" + d)
+					.attr("x",cx)
+					.attr("y",cy+2)
+					.attr("text-anchor", "middle")
+					.attr("font-size", parseInt(dy/2.2) + "px")
+					.attr("fill", "white")
+					.attr("font-family", "sans-serif")
+
+
+			}
+
+		})
+		//TODO prendere solo gli archi tra nodi del k-plesso
+		/*edge.attr("d", function(d){
+			console.log(d.source)
+			if (Object.values(plexes[0]).indexOf(d.source) > -1 && Object.values(plexes[0]).indexOf(d.target) > -1) {
+					console.log("True")
+					var x1 = d3.select("#circle"+d.source).attr("cx")	
+					var y1 = d3.select("#circle"+d.source).attr("cy")
+					var x2 = d3.select("#circle"+d.target).attr("cx")
+					var y2 = d3.select("circle"+d.target).attr("cy")
+
+			var lineGenerator = d3.line()//.curve(d3.curveBundle.beta(1));
+			var lineG = lineGenerator([[x1,y1],[x2,y1],[x2,y2]]);
+			
+			return lineG
+			}
+			else{
+				console.log("false")
+			}
+	}).attr("stroke-linejoin","round");
+	*/
+        function handleMouseOver(d, i) {
+            d3.select(this).select("circle").attr("r", dy*2)
+            d3.select(this).select("text").attr("font-size", parseInt(dy*1.6) + "px")
+          }
+
+      function handleMouseOut(d, i) {
+            d3.select(this).select("circle").attr("r", dy/2)
+            d3.select(this).select("text").attr("font-size", parseInt(dy/2.2) + "px")
+	}
+})
+}
