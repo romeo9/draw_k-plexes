f03f5347825370e31edc258e3bef7e0558fe420f
Antonio Martinelli <antonio.martinelli93@gmail.com>
2017-08-26 15:15:30 +0200

    bugfix ndeConverter + standardization of input files

--- ndeConverter/convert.py
+++ ndeConverter/convert.py
@@ -1,23 +1,28 @@
 import json
 import sys
 
-def convert():
-
-	nameFile = "../dataset/geom.nde";
+def convert(inputFileName):
+	"""file must be in the form:
+		first row: number of nodes
+		next rows: links
+		last row: empty
+	"""
+	nameFile = inputFileName;
+	outputName = nameFile.split("/")[-1]
 
 	inputFile = open(nameFile, "r"); 
 	outputFile = open("output.json", "w");
 
 	data = inputFile.readlines()
 	nodeNumber = int(data[0])
-	data = data[nodeNumber:]
+	data = data[1:]
 	items = []
 	for item in data:
 	  	items.append(item.replace("\n",""))
 
 	edges = items
 	nodes = []
-	for n in range(1,int(nodeNumber+1)):
+	for n in range(0,int(nodeNumber)):
 	  nodes.append(n)
 
 	nodeLabels = []
@@ -35,8 +40,7 @@ def convert():
 	  "links": edgeLabels
 	}
 
-	print(finalData)
-	with open("output.json", "w") as outputFile:
+	with open(outputName[:-4] + ".json", "w") as outputFile:
 	  json.dump(finalData, outputFile, indent=4)
 
-convert()
+convert(sys.argv[1])
